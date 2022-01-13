32e54929263e2756a9536207d847105d67b1e1c5
khuong02 <75919504+khuong02@users.noreply.github.com>
2022-01-13 19:57:56 +0700

    update

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,2 +1,4 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'https://website-chat-server.herokuapp.com/';
+//axios.defaults.baseURL = 'https://website-chat-server.herokuapp.com/';
+axios.defaults.baseURL = 'https://website-server-chat.herokuapp.com/';
+
